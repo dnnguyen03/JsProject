f27ce869eb7feabb797e96b0abb09b9b92b40092
DNNguyen-2003 <dnnguyen3152003@gmail.com>
2022-11-09 14:49:32 +0700

    update

--- scroll/scroll.js
+++ scroll/scroll.js
@@ -1,7 +1,7 @@
 const date = document.getElementById("date");
 date.textContent = new Date().getFullYear();
 const nav = document.getElementById("nav");
-document.addEventListener("scroll", (event) => {
+document.addEventListener("scroll", () => {
   if (window.scrollY > 0) {
     nav.classList.add("scroll-nav");
   } else {
@@ -30,28 +30,54 @@ scrollLink.forEach((link) => {
   link.addEventListener("click", (e) => {
     linksContainer.style.height = 0 + "px";
     const linksHeight = links.getBoundingClientRect().height;
-    const navScroll = nav.classList.contains("scroll-nav");
-    let navHieght = 0;
-    if (x.matches) {
-      navHieght = nav.getBoundingClientRect().height - linksHeight;
-    } else {
-      navHieght = nav.getBoundingClientRect().height;
-    }
-    e.preventDefault();
     const id = e.currentTarget.getAttribute("href").slice(1);
-    const topToSection = document.getElementById(id).offsetTop;
-    let positon = topToSection - navHieght;
+    const navScroll = nav.classList.contains("scroll-nav");
+    let navHeight = 0;
+    if (x.matches) {
+      navHeight = nav.getBoundingClientRect().height - linksHeight;
+    } else {
+      navHeight = nav.getBoundingClientRect().height;
+    }
+    let positon = navHeight;
     if (!navScroll && x.matches) {
-      positon = positon - linksHeight - navHieght;
+      positon = positon + navHeight + linksHeight;
     }
     if (!navScroll && !x.matches) {
-      positon = positon - navHieght;
+      positon = positon + navHeight;
     }
     if (link.classList.contains("btn") && x.matches) {
-      positon = positon - linksHeight;
+      positon = positon + linksHeight;
     }
-    window.scrollTo({
-      top: positon,
-    });
+    document.getElementById(`${id}`).style.scrollMarginTop = positon + "px";
   });
 });
+// const scrollLink = document.querySelectorAll(".scroll-link");
+// scrollLink.forEach((link) => {
+//   link.addEventListener("click", (e) => {
+//     linksContainer.style.height = 0 + "px";
+//     const linksHeight = links.getBoundingClientRect().height;
+//     const navScroll = nav.classList.contains("scroll-nav");
+//     let navHieght = 0;
+//     if (x.matches) {
+//       navHieght = nav.getBoundingClientRect().height - linksHeight;
+//     } else {
+//       navHieght = nav.getBoundingClientRect().height;
+//     }
+//     e.preventDefault();
+//     const id = e.currentTarget.getAttribute("href").slice(1);
+//     const topToSection = document.getElementById(id).offsetTop;
+//     let positon = topToSection - navHieght;
+//     if (!navScroll && x.matches) {
+//       positon = positon - linksHeight - navHieght;
+//     }
+//     if (!navScroll && !x.matches) {
+//       positon = positon - navHieght;
+//     }
+//     if (link.classList.contains("btn") && x.matches) {
+//       positon = positon - linksHeight;
+//     }
+//     window.scrollTo({
+//       top: positon,
+//     });
+//   });
+// });
